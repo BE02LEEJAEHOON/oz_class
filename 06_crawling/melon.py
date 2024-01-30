6d563197093880437d3875777b6ec35ac4dbc383
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-01-30 18:48:41 +0900

    과제

--- 06_crawling/melon.py
+++ 06_crawling/melon.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 header_user = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
-url = 'https://www.melon.com/chart/index.htm'
+url = 'https://www.melon.com/chart/month/index.htm'
 req = requests.get(url, headers=header_user)
 
 html = req.text
@@ -15,18 +15,17 @@ soup = BeautifulSoup(html, "html.parser")
 
 # lst_all = lst50 + lst100
 
-lst_all = soup.find_all(class_=['lst50, lst100'])
+lst_all = soup.find_all(class_=['lst50', 'lst100'])
 
-num = 1
 for i in lst_all:
-    title = i.select_one('.ellipsis.rank01 a')
-    singer = i.select_one('.ellipsis.rank02 a')
-    album = i.select_one('.ellipsis.rank03 a')
+    rank = i.select_one('.rank').text
+    title = i.select_one('.ellipsis.rank01 > span > a').text
+    singer = i.select_one('.ellipsis.rank02 > a').text
+    rank_change_number = i.select('.rank_wrap > span')[1].text
     
-    print(f"순위 : {num}")
-    print(f'제목 : {title.text}')
-    print(f'가수 : {singer.text}')
-    print(f'앨범명 : {album.text}')
+    
+    print(f'순위 : [{rank}]')
+    print(f'순위 변동 : {rank_change_number}')
+    print(f'노래명 : {title}')
+    print(f'가수명 : {singer}')
     print()
-   
-    num += 1 
