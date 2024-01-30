0a332e41899dabdea2e104299fc94866c8ad5efa
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-01-30 17:42:04 +0900

    selenium

--- 06_crawling/naver_6.py
+++ 06_crawling/naver_6.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 
 header_user = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
-url = base_url + search_url
-req = requests.get(url, headers=header_user)
+# # url = base_url + search_url
+# # req = requests.get(url, headers=header_user)
 
-html = req.text
-soup = BeautifulSoup(html, "html.parser")
+# html = req.text
+# soup = BeautifulSoup(html, "html.parser")
 
 #soup.find(class_'link_service', text = '뉴스') # select_one
 #soup.find_all # = select
